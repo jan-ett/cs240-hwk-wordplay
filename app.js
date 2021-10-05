87ee193b6cb51763de7a97258659650db2cfd720
Janett Ramirez <jramirezsevilla@pugetsound.edu>
2021-10-05 16:04:30 -0700

    fixed code to access dictionary correclty and created a wordPlay object to test randomSixLetterWord()

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 class wordPlay{
-    constructor(dictionary) {
-        this.words = this.dictionary;
+    constructor(wordBank) {
+        this.words = dictionary;
         this.sixLetterWords = [];
     }
 
     randomSixLetterWord() {
-        for(let i = 0; i < this.words.size(); i++) {
+        for(let i = 0; i < this.words.length; i++) {
             if(this.words[i].length == 6) {
                 this.sixLetterWords.push(this.words[i]);
             }
@@ -14,5 +14,13 @@ class wordPlay{
         const randomWord = this.sixLetterWords[randomIndex];
         return randomWord;
     }
+
+    //scrambleRootWord(word) {}
 }
-alert("word is not a valid English word (or too short/long)");
+
+const tester = new wordPlay();
+//console.log(tester.words);
+tester.randomSixLetterWord();
+
+// testing to see if console is working
+//alert("word is not a valid English word (or too short/long)");
