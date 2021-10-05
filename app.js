794664c37d884e24aaba4132c139a19047e3bade
Janett Ramirez <jramirezsevilla@pugetsound.edu>
2021-10-04 18:04:21 -0700

    fixed code in randomSixLetterWord to access the array

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ class wordPlay{
     randomSixLetterWord() {
         const sixLetterWords = [];
         for(let i = 0; i < this.words.size(); i++) {
-            if(this.words.get(i) == 6) {
-                sixLetterWords.add(this.words.get(i));
+            if(this.words[i].length == 6) {
+                sixLetterWords.push(this.words[i]);
             }
         }
     }
